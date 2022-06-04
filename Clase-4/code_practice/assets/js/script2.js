6e48f7e9b34783f296f3e706fd8bad9d794d8d4e
felipesanma <felipesmsoto>
2022-06-04 12:53:10 -0400

    object

--- Clase-4/code_practice/assets/js/script2.js
+++ Clase-4/code_practice/assets/js/script2.js
@@ -1,1 +1,29 @@
-console.log("Segundo script, probando el cargado");
+// String
+const nombre = 'Pipe'
+// Number
+const edad = 23
+// Boolean
+const verde = true
+// Array
+const colores = ["rojo", "negro", "azul"]
+// Object
+const cliente = {
+    name: nombre,
+    age: edad,
+    like_weed: verde,
+    fav_colours: colores,
+    address: {
+        street: "Usach",
+        country: "Chile"
+    },
+    saludo: function () {
+        console.log("Hola")
+    }
+}
+
+console.log(nombre, edad, verde)
+console.log(colores)
+console.log(cliente)
+cliente.saludo()
+
+
