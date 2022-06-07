d4640aab9ebb61feb9630e51133cc55723e4d824
felipesanma <felipesmsoto>
2022-06-07 17:15:47 -0400

    tarea4 lista

--- Clase-4/code_practice/assets/js/script2.js
+++ Clase-4/code_practice/assets/js/script2.js
@@ -21,9 +21,46 @@ const cliente = {
     }
 }
 
-console.log(nombre, edad, verde)
-console.log(colores)
-console.log(cliente)
-cliente.saludo()
+// console.log(nombre, edad, verde)
+// console.log(colores)
+// console.log(cliente)
+// cliente.saludo()
+// console.log(cliente.saludo())
+
+function recursive_print_to_100(n) {
+    if (n <= 100) {
+        console.log(n);
+        recursive_print_to_100(n + 1)
+    }
+}
+
+// recursive_print_to_100(1)
+
+function inverse_recusrive_by_2(n) {
+
+    if (n >= 500) {
+        console.log(n);
+        inverse_recusrive_by_2(n - 2)
+    }
+
+}
+
+// inverse_recusrive_by_2(1000)
+
+function suma_notas(array) {
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+        sum += array[i];
+    }
+    return sum
+}
 
 
+function promedio_notas(array) {
+    suma = suma_notas(array)
+    return suma / array.length
+}
+
+const array = [6, 8, 9, 2, 5, 10]
+
+console.log(promedio_notas(array))
