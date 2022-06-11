7064047835999d44d745434209c93497621014b9
felipesanma <felipesmsoto>
2022-06-11 12:38:22 -0400

    for with console timeENd

--- Clase-5/train-code.js
+++ Clase-5/train-code.js
@@ -1,4 +1,7 @@
-let n = 2;
+
+// Condicionales
+
+let n = 1000;
 
 if (n === 10) {
     console.log("Es igual a 10")
@@ -8,9 +11,7 @@ else if (n >= 10 && n <= 20) {
     console.log("Es un valor entre 10 y 20")
 }
 
-else {
-    console.log("No cumplió condiciones anteriores")
-}
+else { }
 
 
 if (n <= 10 || n >= 20) {
@@ -18,3 +19,19 @@ if (n <= 10 || n >= 20) {
 }
 
 console.log(`n tiene el valor ${n}`)
+
+// Ciclos for
+console.time("ciclo for")
+for (let i = 0; i <= 10; i++) {
+    console.log(i)
+}
+console.timeEnd("ciclo for")
+const cadena = "Hola"
+
+for (const value of cadena) {
+    console.log(value);
+}
+
+
+
+
