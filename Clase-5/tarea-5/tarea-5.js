5796c9e98de479ecd17672207f6047c797577d3f
felipesanma <felipesmsoto>
2022-06-17 16:30:47 -0400

    adding cache to fibonacchi

--- Clase-5/tarea-5/tarea-5.js
+++ Clase-5/tarea-5/tarea-5.js
@@ -8,9 +8,23 @@
     c. Números impares entre 0 y 1000.
 
 *Hint: Puedes usar recursividad o algún ciclo o método iterador visto en clase*
-
 */
 
+function fibonacci_sequence_to(n, cache) {
+    cache = cache || {};
+
+    if (cache[n]) {
+        return cache[n];
+    }
+    if (n <= 1) {
+        return n;
+    }
+    return cache[n] = (fibonacci_sequence_to(n - 1, cache) + fibonacci_sequence_to(n - 2, cache));
+}
+
+for (let i = 0; i <= 1000; i++) {
+    console.log(fibonacci_sequence_to(i))
+}
 /*
 2. Del siguiente arreglo de strings retornar otro arreglo con todo a mayúsculas.
 */
