78c9297d6d2594bd5aaa82d178d6bd72af00b0c1
felipesanma <felipesmsoto>
2022-06-18 10:53:00 -0400

    foreach

--- Clase-6/train-code.js
+++ Clase-6/train-code.js
@@ -1,5 +1,16 @@
 function suma_dos_numeros(a, b) {
-    return a + b
+    return a + b;
 }
 
-console.log(suma_dos_numeros(5, 5))
+console.log(suma_dos_numeros(5, 5));
+
+// forEach
+const array = [1, 2, 3, 4, 5];
+array.forEach((numero, i,) => {
+    console.log({ i, numero })
+});
+
+const pokemones = ["Pikachu", "Snake", "Ekans"]
+pokemones.forEach((pokemon, i) => {
+    console.log("pokemones[" + i + "] = " + pokemon);
+});
