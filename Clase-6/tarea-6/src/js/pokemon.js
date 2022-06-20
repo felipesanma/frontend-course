15e2604c3b1c36ee21c59e8f049fbdb716653142
felipesanma <felipesmsoto>
2022-06-20 09:30:47 -0400

    20 random pokemons :)

--- Clase-6/tarea-6/src/js/pokemon.js
+++ Clase-6/tarea-6/src/js/pokemon.js
@@ -1,5 +1,16 @@
-const pokeName = document.querySelector('[data-poke-name]');
-const pokeImg = document.querySelector('[data-poke-img]');
+console.log('Estamos al aire!')
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    let generateBtn = document.querySelector('#random-pokemon');
+    generateBtn.addEventListener('click', renderEverything)
+})
+
+function renderEverything() {
+    let allPokemonContainer = document.querySelector('#poke-container')
+    allPokemonContainer.innerText = "";
+    searchPokemones();
+}
 
 const pokeUrl = 'https://pokeapi.co/api/v2/pokemon/';
 
@@ -7,17 +18,38 @@ const searchPokemones = (pokemones = pokemones_ids()) => {
     pokemones.forEach(pokemon => {
         fetch(`${pokeUrl}${pokemon}`)
             .then(response => response.json())
-            .then(data => renderPokemonData(data))
+            .then(data => renderPokemon(data))
             .catch(err => renderNotFound())
     });
 }
 
-const renderPokemonData = data => {
-    const sprite = data.sprites.front_default;
-    pokeName.textContent = data.name;
-    pokeImg.setAttribute('src', sprite);
+function renderPokemon(pokeData) {
+    let allPokemonContainer = document.getElementById('poke-container');
+    let pokeContainer = document.createElement("div")
+    pokeContainer.classList.add('ui', 'card');
+
+    createPokeImage(pokeData.sprites.front_default, pokeContainer);
+
+    let pokeName = document.createElement('h4')
+    pokeName.innerText = pokeData.name
+
+    let pokeNumber = document.createElement('p')
+    pokeNumber.innerText = `#${pokeData.id}`
+
+    pokeContainer.append(pokeName, pokeNumber);
+    allPokemonContainer.appendChild(pokeContainer);
 }
 
+function createPokeImage(sprite, containerDiv) {
+    let pokeImgContainer = document.createElement('div')
+    pokeImgContainer.classList.add('image')
+
+    let pokeImage = document.createElement('img')
+    pokeImage.srcset = sprite
+
+    pokeImgContainer.append(pokeImage);
+    containerDiv.append(pokeImgContainer);
+}
 
 const pokemones_ids = () => {
     const ids = [];
@@ -30,6 +62,18 @@ const pokemones_ids = () => {
 const random = (low = 1, high = 898) => Math.floor(Math.random() * (1 - low + high)) + low;
 
 const renderNotFound = () => {
-    pokeName.textContent = 'No encontrado';
-    pokeImg.setAttribute('src', 'not_found.png');
+    let allPokemonContainer = document.getElementById('poke-container');
+    let pokeContainer = document.createElement("div")
+    pokeContainer.classList.add('ui', 'card');
+
+    createPokeImage('not_found.png', pokeContainer);
+
+    let pokeName = document.createElement('h4')
+    pokeName.innerText = 'No encontrado';
+
+    let pokeNumber = document.createElement('p')
+    pokeNumber.innerText = "1000"
+
+    pokeContainer.append(pokeName, pokeNumber);
+    allPokemonContainer.appendChild(pokeContainer);
 }
