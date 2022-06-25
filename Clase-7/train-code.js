482c8906d2512e5dc9acf61299d4fbdb3fdd7a1c
felipesanma <felipesmsoto>
2022-06-25 12:36:01 -0400

    class auto

---
+++ Clase-7/train-code.js
@@ -0,0 +1,12 @@
+class Auto {
+    constructor(color, marca, modelo) {
+        this.color = color;
+        this.marca = marca;
+        this.modelo = modelo
+    }
+
+}
+
+const autoPipe = new Auto("Rojo", "Volkswagen", "Voyage");
+
+console.log(autoPipe)
