375c62c5043da7d44bdc30d351520bde4e764e23
felipesanma <felipesmsoto>
2022-07-09 04:11:53 -0400

    pages added

---
+++ Clase-8/my-app-clase-8/src/pages/blog.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Blogs = () => {
+    return (
+        <div
+            style={{
+                display: 'flex',
+                justifyContent: 'Right',
+                alignItems: 'Right',
+                height: '100vh'
+            }}
+        >
+            <h1>Bienvenido al blog de los chasquilla engineers!</h1>
+        </div>
+    );
+};
+
+export default Blogs;
